ca41dfa0d390a01ff8a1a74bf8d24b41f5e11c7d
Miha Dimec <miha.dimec@visionect.com>
2018-03-16 13:07:06 +0100

    almost all

--- pkg/allrecipes/allrecipes.go
+++ pkg/allrecipes/allrecipes.go
@@ -1,39 +1,34 @@
 package main
 
 import (
-    "os"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-    "net/url"
-    "strings"
+	"net/url"
+	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
-type InstructionType struct {
-	ImageURL    string `json:"image_url"`
-	Instruction string `json:"instruction"`
-}
-
 type Recipe struct {
-	RecipeID     string            `json:"recipe_id"`
-	Publisher    string            `json:"publisher"`
-	SourceURL    string            `json:"source_url"`
-	Title        string            `json:"title"`
-	ImageURL     string            `json:"image_url"`
-	Description  string            `json:"description"`
-	Ingredients  []string          `json:"ingredients"`
-	Instructions []InstructionType `json:"instructions"`
-	Tips         []string          `json:"tips"`
+	RecipeID     string   `json:"recipe_id"`
+	Publisher    string   `json:"publisher"`
+	SourceURL    string   `json:"source_url"`
+	Title        string   `json:"title"`
+	ImageURL     string   `json:"image_url"`
+	Description  string   `json:"description"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	Tips         []string `json:"tips"`
 }
 
 func delNewLine(s string) string {
-    return strings.Replace(
-        strings.Replace(s, "\n", "", -1),
-        "\r", "", -1)
+	return strings.Replace(
+		strings.Replace(s, "\n", "", -1),
+		"\r", "", -1)
 }
 
 func checkAttr(attr []html.Attribute, key, val string) bool {
@@ -54,25 +49,25 @@ func getAttrVal(attr []html.Attribute, key string) string {
 	return ""
 }
 
-func getRecipe(recipeUrl string) (Recipe, error) {
-    // get recipe id from url
-    u, err := url.Parse(recipeUrl)
+func GetRecipe(recipeUrl string) (Recipe, error) {
+	// get recipe id from url
+	u, err := url.Parse(recipeUrl)
 	if err != nil {
 		return Recipe{}, err
 	}
-    if u.Host != "allrecipes.com" {
-        return Recipe{}, errors.New("expected allrecipes.com host name")
-    }
-    //remove Qury part from URL
-    u.RawQuery = ""
+	if u.Host != "allrecipes.com" {
+		return Recipe{}, errors.New("expected allrecipes.com host name")
+	}
+	//remove Qury part from URL
+	u.RawQuery = ""
 
-    // parse html
-    resp, err := http.Get(u.String())
+	// parse html
+	resp, err := http.Get(u.String())
 	if err != nil {
 		return Recipe{}, err
 	}
 	defer resp.Body.Close()
-    ret := Recipe{RecipeID: u.String(), SourceURL: u.String()}
+	ret := Recipe{RecipeID: u.String(), SourceURL: u.String()}
 
 	z := html.NewTokenizer(resp.Body)
 endloop:
@@ -88,13 +83,13 @@ endloop:
 			token := z.Token()
 			if token.DataAtom == atom.H1 &&
 				checkAttr(token.Attr, "itemprop", "name") {
-                // <h1 class="recipe-summary__h1" itemprop="name">Spaghetti Pie I</h1>
+				// <h1 class="recipe-summary__h1" itemprop="name">Spaghetti Pie I</h1>
 				tt := z.Next()
 				switch tt {
 				case html.TextToken:
 					token = z.Token()
-					fmt.Println("title>", token.Data)
 					ret.Title = delNewLine(html.UnescapeString(token.Data))
+					fmt.Println("Title>", ret.Title)
 				case html.ErrorToken:
 					return Recipe{}, z.Err()
 				default:
@@ -102,13 +97,13 @@ endloop:
 				}
 			} else if token.DataAtom == atom.Span &&
 				checkAttr(token.Attr, "itemprop", "author") {
-                // <span class="submitter__name" itemprop="author">Kimberley</span>
+				// <span class="submitter__name" itemprop="author">Kimberley</span>
 				tt := z.Next()
 				switch tt {
 				case html.TextToken:
 					token = z.Token()
-					fmt.Println("author>", token.Data)
 					ret.Publisher = delNewLine(html.UnescapeString(token.Data))
+					fmt.Println("Publisher>", ret.Publisher)
 				case html.ErrorToken:
 					return Recipe{}, z.Err()
 				default:
@@ -116,20 +111,19 @@ endloop:
 				}
 			} else if token.DataAtom == atom.Div &&
 				checkAttr(token.Attr, "itemprop", "description") {
-                // <div class="submitter__description" itemprop="description"> "Family favorite. Serve with lemon wedges."</div>
+				// <div class="submitter__description" itemprop="description"> "Family favorite. Serve with lemon wedges."</div>
 				tt := z.Next()
 				switch tt {
 				case html.TextToken:
 					token = z.Token()
-                    fmt.Println(token)
-					fmt.Println("description>", token.Data)
 					ret.Description = delNewLine(html.UnescapeString(token.Data))
+					fmt.Println("Description>", ret.Description)
 				case html.ErrorToken:
 					return Recipe{}, z.Err()
 				default:
 					return Recipe{}, errors.New("allrecipes parser: author name was expected here")
 				}
-			} else	if token.DataAtom == atom.Span &&
+			} else if token.DataAtom == atom.Span &&
 				checkAttr(token.Attr, "itemprop", "ingredients") {
 				// did we hit one of the ingredients
 				// <span class="recipe-ingred_txt added" ... itemprop="ingredients">
@@ -138,9 +132,9 @@ endloop:
 				switch tt {
 				case html.TextToken:
 					token = z.Token()
-					fmt.Println("ingredient>", token.Data)
-					ret.Ingredients = append(ret.Ingredients, 
-                        delNewLine(html.UnescapeString(token.Data)))
+					ret.Ingredients = append(ret.Ingredients,
+						delNewLine(html.UnescapeString(token.Data)))
+					fmt.Println("Ingredient>", ret.Ingredients[len(ret.Ingredients)-1])
 				case html.ErrorToken:
 					return Recipe{}, z.Err()
 				default:
@@ -156,25 +150,24 @@ endloop:
 				switch tt {
 				case html.TextToken:
 					token = z.Token()
-					fmt.Println("instruction>", token.Data)
 					ret.Instructions = append(ret.Instructions,
-						InstructionType{
-                            Instruction: delNewLine(html.UnescapeString(token.Data))})
+						delNewLine(html.UnescapeString(token.Data)))
+					fmt.Println("Instruction", ret.Instructions[len(ret.Instructions)-1])
 				case html.ErrorToken:
 					return Recipe{}, z.Err()
 				default:
 					return Recipe{}, errors.New("allrecipes parser: instruction text was expected here")
 				}
 			}
-        case html.SelfClosingTagToken:
+		case html.SelfClosingTagToken:
 			token := z.Token()
 			if token.DataAtom == atom.Meta &&
-                checkAttr(token.Attr, "property", "og:image") {
-                // <meta property="og:image" content="https://images.media-allrecipes.com/userphotos/560x315/726090.jpg" />
-                imgURL := getAttrVal(token.Attr, "content")
-				fmt.Println("image>", imgURL)
-                ret.ImageURL = imgURL
-            }
+				checkAttr(token.Attr, "property", "og:image") {
+				// <meta property="og:image" content="https://images.media-allrecipes.com/userphotos/560x315/726090.jpg" />
+				imgURL := getAttrVal(token.Attr, "content")
+				fmt.Println("Image>", imgURL)
+				ret.ImageURL = imgURL
+			}
 
 		}
 
@@ -183,9 +176,9 @@ endloop:
 }
 
 func main() {
-    url := "http://allrecipes.com/recipe/231495/texas-boiled-beer-shrimp/"
-	//url := "http://allrecipes.com/recipe/11772/spaghetti-pie-i/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=231495%20referringContentType%3Drecipe"
-	recipe, err := getRecipe(url)
+	//url := "http://allrecipes.com/recipe/231495/texas-boiled-beer-shrimp/"
+	url := "http://allrecipes.com/recipe/11772/spaghetti-pie-i/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=231495%20referringContentType%3Drecipe"
+	recipe, err := GetRecipe(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err) // TODO stderr
 		return
